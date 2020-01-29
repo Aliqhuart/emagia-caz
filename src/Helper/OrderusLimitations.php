029ec6fef7896ecd68b0d8578547157be45a32c2
Corin Zotica <alexandru.corin.zotica@gmail.com>
2020-01-29 17:09:36 +0200

    Added the Orderus character stats limitations

---
+++ src/Helper/OrderusLimitations.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Emagia\Helper;
+
+/**
+ * Class OrderusLimitations
+ *
+ * @package Emagia\Helper
+ * @author  Corin Zotica <corin.zotica@dcsplus.net>
+ */
+class OrderusLimitations
+{
+    public const MIN_HEALTH   = 70;
+
+    public const MAX_HEALTH   = 100;
+
+    public const MIN_STRENGTH = 70;
+
+    public const MAX_STRENGTH = 80;
+
+    public const MIN_DEFENCE  = 45;
+
+    public const MAX_DEFENCE  = 55;
+
+    public const MIN_SPEED    = 40;
+
+    public const MAX_SPEED    = 50;
+
+    public const MIN_LUCK     = 10;
+
+    public const MAX_LUCK     = 30;
+}
