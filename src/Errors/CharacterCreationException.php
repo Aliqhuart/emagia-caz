7393d11505d8007e59d1abe1877790b6962c775c
Corin Zotica <alexandru.corin.zotica@gmail.com>
2020-01-29 17:05:48 +0200

    Implemented the character creation error class

---
+++ src/Errors/CharacterCreationException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Emagia\Errors;
+
+/**
+ * Class CharacterCreationException
+ *
+ * @package Emagia\Errors
+ * @author  Corin Zotica <corin.zotica@dcsplus.net>
+ */
+class CharacterCreationException extends AbstractError
+{
+}
