c220a0a84c389e47576f2865cb6691e588ecad77
Corin Zotica <alexandru.corin.zotica@gmail.com>
2020-01-29 17:05:19 +0200

    Implemented the base abstract error class

---
+++ src/Errors/AbstractError.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Emagia\Errors;
+
+use Exception;
+
+/**
+ * Class AbstractError
+ *
+ * @package Emagia\Errors
+ * @author  Corin Zotica <corin.zotica@dcsplus.net>
+ */
+abstract class AbstractError extends Exception
+{
+}
