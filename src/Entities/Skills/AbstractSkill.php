9012cd3825dadbda5637f3562b6e13ce918cee67
Corin Zotica <alexandru.corin.zotica@gmail.com>
2020-01-29 16:33:41 +0200

    Base Abstract Skill class

---
+++ src/Entities/Skills/AbstractSkill.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Emagia\Entities\Skills;
+
+/**
+ * Class AbstractSkill
+ *
+ * @package Emagia\Entities\Skills
+ * @author  Corin Zotica <corin.zotica@dcsplus.net>
+ */
+abstract class AbstractSkill
+{
+}
