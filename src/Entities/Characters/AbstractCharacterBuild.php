87ef376d37af32cab782edcd7294850c7a66589b
Corin Zotica <alexandru.corin.zotica@gmail.com>
2020-01-29 16:41:26 +0200

    Getters & setters for the abstract character

--- src/Entities/Characters/AbstractCharacterBuild.php
+++ src/Entities/Characters/AbstractCharacterBuild.php
@@ -46,4 +46,156 @@ abstract class AbstractCharacterBuild
      * @var AbstractSkill[]
      */
     protected $skillList;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return AbstractCharacterBuild
+     */
+    public function setName(string $name): AbstractCharacterBuild
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getHealth(): int
+    {
+        return $this->health;
+    }
+
+    /**
+     * @param int $health
+     *
+     * @return AbstractCharacterBuild
+     */
+    public function setHealth(int $health): AbstractCharacterBuild
+    {
+        $this->health = $health;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStrength(): int
+    {
+        return $this->strength;
+    }
+
+    /**
+     * @param int $strength
+     *
+     * @return AbstractCharacterBuild
+     */
+    public function setStrength(int $strength): AbstractCharacterBuild
+    {
+        $this->strength = $strength;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDefence(): int
+    {
+        return $this->defence;
+    }
+
+    /**
+     * @param int $defence
+     *
+     * @return AbstractCharacterBuild
+     */
+    public function setDefence(int $defence): AbstractCharacterBuild
+    {
+        $this->defence = $defence;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSpeed(): int
+    {
+        return $this->speed;
+    }
+
+    /**
+     * @param int $speed
+     *
+     * @return AbstractCharacterBuild
+     */
+    public function setSpeed(int $speed): AbstractCharacterBuild
+    {
+        $this->speed = $speed;
+
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLuck(): int
+    {
+        return $this->luck;
+    }
+
+    /**
+     * @param int $luck
+     *
+     * @return AbstractCharacterBuild
+     */
+    public function setLuck(int $luck): AbstractCharacterBuild
+    {
+        $this->luck = $luck;
+
+        return $this;
+    }
+
+    /**
+     * @return AbstractSkill[]
+     */
+    public function getSkillList(): array
+    {
+        return $this->skillList;
+    }
+
+    /**
+     * @param AbstractSkill[] $skillList
+     *
+     * @return AbstractCharacterBuild
+     */
+    public function setSkillList(array $skillList): AbstractCharacterBuild
+    {
+        $this->skillList = $skillList;
+
+        return $this;
+    }
+
+    /**
+     * @param AbstractSkill $skill
+     *
+     * @return $this
+     */
+    public function addSkill(AbstractSkill $skill): self
+    {
+        $this->skillList[] = $skill;
+
+        return $this;
+    }
 }
