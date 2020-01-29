55269b96d08e3b64f0b7d76d61db9e4fe37b0c37
Corin Zotica <alexandru.corin.zotica@gmail.com>
2020-01-29 18:22:48 +0200

    Added the wild beast limitations

---
+++ src/Helper/WildBeastLimitations.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Emagia\Helper;
+
+class WildBeastLimitations
+{
+    public const MIN_HEALTH   = 60;
+
+    public const MAX_HEALTH   = 90;
+
+    public const MIN_STRENGTH = 60;
+
+    public const MAX_STRENGTH = 90;
+
+    public const MIN_DEFENCE  = 40;
+
+    public const MAX_DEFENCE  = 60;
+
+    public const MIN_SPEED    = 40;
+
+    public const MAX_SPEED    = 60;
+
+    public const MIN_LUCK     = 25;
+
+    public const MAX_LUCK     = 40;
+}
