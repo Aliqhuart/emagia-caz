f0be6b49369bd0d30c7fb9c9d008859e57cdbb51
Corin Zotica <alexandru.corin.zotica@gmail.com>
2020-01-29 20:45:43 +0200

    Added a new exception for the battle event

---
+++ src/Errors/BattleException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Emagia\Errors;
+
+/**
+ * Class BattleException
+ *
+ * @package Emagia\Errors
+ * @author  Corin Zotica <corin.zotica@dcsplus.net>
+ */
+class BattleException extends AbstractError
+{
+}
