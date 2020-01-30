09d2404cf6f6154d32542237ecac1b137580b8d9
Corin Zotica <alexandru.corin.zotica@gmail.com>
2020-01-30 17:59:50 +0200

    Updated the character skill limitation enums

--- src/Helper/OrderusLimitations.php
+++ src/Helper/OrderusLimitations.php
@@ -6,27 +6,27 @@ namespace Emagia\Helper;
  * Class OrderusLimitations
  *
  * @package Emagia\Helper
- * @author  Corin Zotica <corin.zotica@dcsplus.net>
+ * @author  Corin Zotica <alexandru.corin.zotica@gmail.com>
  */
 class OrderusLimitations
 {
-    public const MIN_HEALTH   = 70;
+    const MIN_HEALTH   = 70;
 
-    public const MAX_HEALTH   = 100;
+    const MAX_HEALTH   = 100;
 
-    public const MIN_STRENGTH = 70;
+    const MIN_STRENGTH = 70;
 
-    public const MAX_STRENGTH = 80;
+    const MAX_STRENGTH = 80;
 
-    public const MIN_DEFENCE  = 45;
+    const MIN_DEFENCE  = 45;
 
-    public const MAX_DEFENCE  = 55;
+    const MAX_DEFENCE  = 55;
 
-    public const MIN_SPEED    = 40;
+    const MIN_SPEED    = 40;
 
-    public const MAX_SPEED    = 50;
+    const MAX_SPEED    = 50;
 
-    public const MIN_LUCK     = 10;
+    const MIN_LUCK     = 10;
 
-    public const MAX_LUCK     = 30;
+    const MAX_LUCK     = 30;
 }
--- src/Helper/WildBeastLimitations.php
+++ src/Helper/WildBeastLimitations.php
@@ -2,25 +2,31 @@
 
 namespace Emagia\Helper;
 
+/**
+ * Class WildBeastLimitations
+ *
+ * @package Emagia\Helper
+ * @author  Corin Zotica <alexandru.corin.zotica@gmail.com>
+ */
 class WildBeastLimitations
 {
-    public const MIN_HEALTH   = 60;
+    const MIN_HEALTH   = 60;
 
-    public const MAX_HEALTH   = 90;
+    const MAX_HEALTH   = 90;
 
-    public const MIN_STRENGTH = 60;
+    const MIN_STRENGTH = 60;
 
-    public const MAX_STRENGTH = 90;
+    const MAX_STRENGTH = 90;
 
-    public const MIN_DEFENCE  = 40;
+    const MIN_DEFENCE  = 40;
 
-    public const MAX_DEFENCE  = 60;
+    const MAX_DEFENCE  = 60;
 
-    public const MIN_SPEED    = 40;
+    const MIN_SPEED    = 40;
 
-    public const MAX_SPEED    = 60;
+    const MAX_SPEED    = 60;
 
-    public const MIN_LUCK     = 25;
+    const MIN_LUCK     = 25;
 
-    public const MAX_LUCK     = 40;
+    const MAX_LUCK     = 40;
 }
