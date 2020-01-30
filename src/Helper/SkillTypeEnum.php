6c348ec101f231f5971edf02a45701a9be1c2b78
Corin Zotica <alexandru.corin.zotica@gmail.com>
2020-01-30 17:57:22 +0200

    Added the Skill Type Enum class in order to better define skills

---
+++ src/Helper/SkillTypeEnum.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Emagia\Helper;
+
+/**
+ * Class SkillTypeEnum
+ *
+ * @package Emagia\Helper
+ * @author  Corin Zotica <alexandru.corin.zotica@gmail.com>
+ */
+class SkillTypeEnum
+{
+    const ATTACK_ROLL = 'attack-roll';
+
+    const DEFENCE = 'defence';
+
+    const STRENGTH = 'strength';
+}
