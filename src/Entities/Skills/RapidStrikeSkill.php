5300084be4f5926b79f264cfe260c764a382406f
Corin Zotica <alexandru.corin.zotica@gmail.com>
2020-01-30 17:57:59 +0200

    Updated the rapid strike skill to use the enum for standardization

--- src/Entities/Skills/RapidStrikeSkill.php
+++ src/Entities/Skills/RapidStrikeSkill.php
@@ -2,11 +2,13 @@
 
 namespace Emagia\Entities\Skills;
 
+use Emagia\Helper\SkillTypeEnum;
+
 /**
  * Class RapidStrikeSkill
  *
  * @package Emagia\Entities\Skills
- * @author  Corin Zotica <corin.zotica@dcsplus.net>
+ * @author  Corin Zotica <alexandru.corin.zotica@gmail.com>
  */
 class RapidStrikeSkill extends AbstractSkill
 {
@@ -17,7 +19,7 @@ class RapidStrikeSkill extends AbstractSkill
     {
         parent::__construct(
             'Rapid Strike',
-            'attack',
+            SkillTypeEnum::ATTACK_ROLL,
             2.0,
             10
         );
